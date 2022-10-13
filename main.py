d859091b01296b24f15f0fc4ad3bc3c9c3656c91
veilleuxd <veilleuxd@educ.college-montreal.qc.ca>
2022-10-13 09:17:31 -0400

    adding the menu

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ import random()
 
 player_hp = 20
 
+game_menu = input("Combat de Monstres\nPour commencer, écrives 'start'\nPour lire les règles, écrives 'rules'\nPour quitter, écrivez 'leave'\n--->")
+
+if game_menu = 'rules' :
+    print("Please read later")
