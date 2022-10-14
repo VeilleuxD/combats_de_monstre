03b0c650c63e7cd7c03442024e4bc6341186371c
veilleuxd <veilleuxd@educ.college-montreal.qc.ca>
2022-10-14 10:42:24 -0400

    Menu du jeux fini!

--- main.py
+++ main.py
@@ -5,11 +5,40 @@ une série de combats de monstres générés aléatoirement
 amusant
 """
 
-import random()
+import random
+from color import Colors
+from time import sleep
 
-player_hp = 20
+def fight():
+    niveau_hp = random.randint(15, 25)
 
-game_menu = input("Combat de Monstres\nPour commencer, écrives 'start'\nPour lire les règles, écrives 'rules'\nPour quitter, écrivez 'leave'\n--->")
 
-if game_menu = 'rules' :
-    print("Please read later")
+def menu():
+    boucle_menu = True
+    player_hp = 20
+
+    while boucle_menu:
+        print(Colors.CYAN + Colors.BOLD + "Combat de Monstres\n\n\n")
+        sleep(1)
+        print(Colors.LIGHT_GREEN + Colors.ITALIC + "Pour commencer, écrivez 'start'")
+        sleep(0.5)
+        print(Colors.YELLOW + Colors.ITALIC + "\n\nPour lire les règles, écrivez 'rules'\n\n")
+        sleep(0.5)
+        print(Colors.LIGHT_RED + Colors.ITALIC + "Pour quitter, écrivez 'leave'\n")
+        sleep(0.5)
+        game_menu = input(Colors.CYAN + Colors.BOLD + "--->")
+
+        if game_menu == 'rules':
+            print("Please read later")
+            boucle_menu = True
+        elif game_menu == 'leave':
+            print("Au revoir!")
+            boucle_menu = False
+        elif game_menu == 'start':
+            print("Oh non, un monstre!")
+            boucle_menu = True
+        else:
+            boucle_menu = True
+
+
+menu()
